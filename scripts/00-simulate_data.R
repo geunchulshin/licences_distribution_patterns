49b3fa3262c0a73399913ed5c797467a1321b963
Geunchul Shin <joey.shin@mail.utoronto.ca>
2024-01-22 01:03:54 +0000

    Editing fixed default author/date to mine

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
 # Purpose: Simulates the dataset that is used for the analysis.
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Geunchul Shin
+# Date: January 21 2024
+# Contact: joey.shin@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
-# Any other information needed? No
 
 
 #### Workspace setup ####
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,10 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Geunchul Shin
+# Date: January 21 2024
+# Contact: joey.shin@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 01-download_data.R
 
 #### Workspace setup ####
 library(tidyverse)
